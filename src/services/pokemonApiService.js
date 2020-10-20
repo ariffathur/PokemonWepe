e9c4941a3fee7001c164bf3cb89a4771cc437bcb
Arif Fathurohman <mariffathurohman@gmail.com>
2020-10-20 21:57:57 +0700

    config pokemon api service

---
+++ src/services/pokemonApiService.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const pokedexApiClient = axios.create({
+  baseURL: "https://pokeapi.co/api/v2/",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+function fetchPokemonList(params) {
+  return pokedexApiClient
+    .get(`pokemon/${params}`)
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      return error.response;
+    });
+}
+
+function fetchPokemonDetail(url) {
+  return pokedexApiClient
+    .get(url)
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      return error.response;
+    });
+}
+
+export const wecareService = {
+  fetchPokemonList,
+  fetchPokemonDetail,
+};
